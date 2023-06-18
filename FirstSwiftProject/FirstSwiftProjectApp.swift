467d010cce8691cd24ee72a2538e24daf42927cd
mushyoom <mushyoom@gmail.com>
2023-06-18 18:53:00 +0900

    Initial Commit

---
+++ FirstSwiftProject/FirstSwiftProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstSwiftProjectApp.swift
+//  FirstSwiftProject
+//
+//  Created by Shyann on 2023/06/18.
+//
+
+import SwiftUI
+
+@main
+struct FirstSwiftProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
